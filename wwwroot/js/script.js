d88b884d95a7dfc43f7b91d2afdc2281c76c1a78
Tore Aurstad <tore.aurstad@hemit.no>
2020-12-28 22:19:03 +0100

    Init

---
+++ wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿var clock;
+function startTime(element) {
+	let timeString = new Date().toLocaleTimeString('nb-No', { hour: 'numeric', hour12: false, minute: 'numeric', second: 'numeric' });
+	element.innerHTML = timeString;
+	clock = setTimeout(startTime.bind(null, element), 1000);
+}
+function stopTime() {
+	clearTimeout(clock);
+}
